856c025749edee20fef660df61316414972fd6bd
gergelydanyi <gergely.danyi@gmail.com>
2024-11-14 13:01:42 +0100

    Change Shape class from abstract to virtual.

--- PatternMachine/Shape.cpp
+++ PatternMachine/Shape.cpp
@@ -4,7 +4,18 @@ namespace PatternMachine
 {
 
 	// TODO: add inner color
-
+	bool Shape::isEditing()
+	{
+		return isSizing || isMoving;
+	}
+	void Shape::StartSizing(POINT)
+	{}
+	void Shape::Sizing(POINT, POINT)
+	{}
+	void Shape::StopSizing()
+	{
+		isSizing = false;
+	}
 	RECT Shape::BoundingRectangle()
 	{
 		RECT rc;
--- PatternMachine/Shape.h
+++ PatternMachine/Shape.h
@@ -11,10 +11,10 @@ public:
 	POINT anchor;
 	RECT rect = { 0, 0, 100, 100 };
 	virtual RECT BoundingRectangle();
-	virtual bool isEditing() = 0;
-	virtual void StartSizing(POINT) = 0;
-	virtual void Sizing(POINT, POINT) = 0;
-	virtual void StopSizing() = 0;
+	virtual bool isEditing();
+	virtual void StartSizing(POINT);
+	virtual void Sizing(POINT, POINT);
+	virtual void StopSizing();
 	bool isSizing = false;
 	bool isMoving = false;
 	bool isDrawn = true;
